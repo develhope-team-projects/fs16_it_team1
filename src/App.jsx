46e7cac4a5f6e56e33e20fb50501b77aa71216d4
AlessioNatale <alessionatale12@gmail.com>
2023-12-04 22:53:43 +0100

    risolto posizione navbar, stile e mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,37 @@ import Login from "./Login"
 import { Home } from "./Components/Home.jsx";
 import { Adventure } from "./Components/Adventure.jsx";
 import { Game } from "./Components/Game.jsx";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './Components/NavBar';
 import { Rules } from "./Rules"
 import { About } from "./Components/About";
 import "./Style/App.css"
 import "./Home.css"
 import './style.css'
+import Sidebar from "./Components/Sidebar.jsx";
+import { useState } from "react";
   
 
 
 function App() {
+
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+
+  const location = useLocation();
+  // console.log(location);
+
+  const funzioneDiChiusura = () => {
+    setSidebarVisible(false)
+  }
+
+  function funzioneAperturaSideBar (){
+    setSidebarVisible(true)
+  }
+
   return (
-
-
-    
     <>
-    <Navbar/>
+    {sidebarVisible && <Sidebar onClose={funzioneDiChiusura}/>}
+    {location && location.pathname !== '/' && <Navbar sidebarVisible={sidebarVisible} onOpenSideBar={funzioneAperturaSideBar}/>}
       <Routes>
         <Route path="/" element={<Login />} />
         {/* <Route path="/:user/:id" element={<Game />} /> */}
@@ -29,7 +43,6 @@ function App() {
         <Route path="/game/:hero" element={<Game />} />
       </Routes>
     </>
-      
   )
 }
 
---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Sidebar(props) {
+
+
+  const navigate = useNavigate();
+
+  return(
+    <div className={`sidebar visible `}>
+
+    <div className="posizione-x">
+      <div class="menu-icon-hamburger-x" onClick={props.onClose}>
+        <span className="line"></span>
+        <span className="line"></span>
+      </div>
+    </div>
+
+    <div className="posizione-sidebar">
+      <div className="Tab" onClick={()=>{
+        navigate("/home")
+        props.onClose()
+      }}>
+          Home
+      </div>
+      <div className="Tab"onClick={()=>{
+        navigate("/About")
+        props.onClose()
+      }}>
+          About
+      </div>
+      <div className="Tab"onClick={()=>{
+        navigate("/Rules")
+        props.onClose()
+      }}>
+          Rules
+      </div>
+    </div>
+
+  </div>
+
+
+  
+
+  )
+}
+
+export default Sidebar;
